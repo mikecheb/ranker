1d3290bcf0ee306b181362ac58033c18bffca508
mikecheb <mike@mikechen.is>
2021-02-06 21:58:56 -0800

    Update site domain

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Base application title used in document.title
-  title: 'React pages boilerplate',
+  title: 'Ranker',
 
   // use cname option to add CNAME file to webpack build
   // CNAME file allows to use custom domain names with gh-pages, example:
   // cname: 'omatsuri.app'
-  cname: null,
+  cname: "mikechen.is",
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: '/',
+  repoPath: "/ranker",
 };
