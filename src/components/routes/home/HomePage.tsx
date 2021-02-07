6db898cc152f57b3ca96274c0c29651f30674714
mikecheb <mike@mikechen.is>
2021-02-07 00:24:07 -0800

    Render sample villagers

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DATASET } from '/src/data';
+import "./style.css";
+
+interface Villager {
+  name: string;
+  image: string;
+}
+
+function VillagerTile(props: {
+  selectedVillagers: Set<string>,
+  villager: Villager
+}) {
+  return (
+    <div className="villager-tile">
+      <div className="villager-tile__image-container">
+        <img className="villager-tile__image" src={props.villager.image} />
+      </div>
+      <div className="villager-tile__name">
+      {props.villager.name}</div>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const [selectedVillagers, setSelectedVillagers] = useState(new Set());
+
+  const data = DATASET;
+  const villagerTiles = [];
+  for (const villager of data) {
+    villagerTiles.push(<VillagerTile villager={villager} />);
+  }
+
   return (
     <div>
-      <h1 style={{ fontSize: 50, fontWeight: 'bold', textAlign: 'center' }}>
-        React Pages Boilerplate!
-      </h1>
+      <div>
+        This is the picker section
+        {villagerTiles}
+
+      </div>
+      <div>This is the favorites list</div>
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,22 @@
+export const DATASET = [{
+    name: "Raymond",
+    image: "https://dodo.ac/np/images/2/2a/Raymond_NH.png",
+}, {
+    name: "Deena",
+    image: "https://dodo.ac/np/images/thumb/b/b8/Deena_NH.png/380px-Deena_NH.png",
+}, {
+    name: "Jambette",
+    image: "https://dodo.ac/np/images/thumb/3/35/Jambette_NH.png/378px-Jambette_NH.png"
+}, {
+    name: "Beardo",
+    image: "https://dodo.ac/np/images/thumb/c/c0/Beardo_NH.png/335px-Beardo_NH.png"
+}, {
+    name: "Rodney",
+    image: "https://dodo.ac/np/images/thumb/3/35/Rodney_NH.png/410px-Rodney_NH.png",
+}, {
+    name: "Jitters",
+    image: "https://dodo.ac/np/images/a/a6/Jitters_NH.png",
+}, {
+    name: "Tangy",
+    image: "https://dodo.ac/np/images/thumb/6/6f/Tangy_NH.png/285px-Tangy_NH.png",
+}];
